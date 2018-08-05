3521650ecd2f44346dbb8d9288058b6bb968b5ae
Laura Daza <la.daza10@uniandes.edu.co>
2018-08-05 12:42:13 -0500

    Update Read_data.py

--- Read_data.py
+++ Read_data.py
@@ -12,29 +12,22 @@ from scipy.ndimage.interpolation import zoom
 import nibabel as nib
 import time
 import ConfigParser
-from joblib import Parallel, delayed
 
 def get_list(mode):
 	filenames = [[] for _ in range(gb.num_ch)]
 	labelnames = []
-	if mode == 'train':
-		root = gb.impath
-	else:
-		root = gb.testpath
-
 	for i in range(gb.num_ch):
-		name = root + gb.ends[i]
+		name = gb.impath + gb.ends[i]
 		with open(name) as f:
 			filenames[i] = f.readlines()
 		
 		filenames[i] = [x.strip() for x in filenames[i]] 
 	filenames = map(list, zip(*filenames))
 
-	if mode == 'train':
-		name = root + gb.ends[-1]
-		with open(name) as f:
-			labelnames = f.readlines()
-		labelnames = [x.strip() for x in labelnames]
+	name = gb.impath + gb.ends[-1]
+	with open(name) as f:
+		labelnames = f.readlines()
+	labelnames = [x.strip() for x in labelnames] 
 	return filenames, labelnames
 
 def downsampled_patches(image, voxel, downsample):
@@ -52,89 +45,90 @@ def extract_patch(image, voxel, size_p, channels):
 	im_size = image[0].shape
 	difference = im_size - voxel - 1
 	difference = np.asarray(difference)
-	center = (size_p - size_p%2)/2 + 1
+	min_diff = (size_p - size_p%2)/2 + 1
 
-	if difference.min() <= center or any(difference >= (np.asarray(im_size) - center)):
-		image  = [np.pad(i, center, 'constant', constant_values=(0)) for i in image] 
+	if difference.min() <= min_diff or any(difference >= (np.asarray(im_size) - min_diff)):   
+		# Posible modification: paddding with the size of the difference     
+		image  = [np.pad(i, (min_diff), 'constant', constant_values=(0)) for i in image] 
 		v1 = voxel
 	else:
-		v1 = voxel - center
+		v1 = voxel - min_diff
 
 	v1 = v1.astype(int)
 	v2 = v1 + size_p
 	patch_list = []
 	for i in range(channels):
 		patch = image[i][v1[0]:v2[0], v1[1]:v2[1], v1[2]:v2[2]]
+		if len(np.unique(patch.shape)) > 1:
+			pdb.set_trace()
 		patch_list.append(patch)
 	return np.stack(patch_list, axis=-1)
 
 def read_image(path, label):
 	img = nib.load(path)
-	image = np.squeeze(img.get_data())
-	# image = np.transpose(image, [2,1,0])
+	im = img.get_data()
+
+	# Binarization of the labels (whole tumor)
+	if label == 1 and gb.num_classes == 2:
+		image = (image != 0)*1 
 	return image	
 
-def make_patches(label, voxel, image, size_patches, mode):
+def make_patches(label, voxel, sizes, image, mode):
 	if mode == 'train':
 		patches = [[] for _ in range(gb.num_paths+1)]
 	else:
 		patches = [[] for _ in range(gb.num_paths)]
 
-	input_im = image
-	input_voxel = np.asarray(voxel)	
 	for pathway in range(gb.num_paths):
-		if pathway != 0:
+		if pathway == 0:
+			input_im = image
+			input_voxel = np.asarray(voxel)
+		else:
 			input_im, input_voxel = downsampled_patches(image, voxel, gb.downsample[pathway-1])
 		
 		for im in range(len(voxel)):
-			temp_im = extract_patch(input_im, input_voxel[im], size_patches, gb.num_ch)
+			temp_im = extract_patch(input_im, input_voxel[im], sizes[0][pathway], gb.num_ch)
 			patches[pathway].append(temp_im)
 
 	if mode == 'train':
+		temp_l = []
 		for i in range(len(voxel)):
-			patches[-1].append(np.squeeze(extract_patch([label], np.asarray(voxel[i]), size_patches, 1)))
+			patches[-1].append(np.squeeze(extract_patch([label], np.asarray(voxel[i]), sizes[1], 1)))
+
 	return patches
 
-def voxel_selection(label, image):
-	annot = np.unique(label)
-	batch = gb.patches_per_patient/(len(annot))
-	extra = gb.patches_per_patient % len(annot) 
-	nop = np.argwhere(image[0] == image[0].min()) # Background voxels
+def voxel_selection(label, image, sizes):
+	batch = gb.patches_per_patient/2
+	nop = np.argwhere(image[0] == image[0].min())
 	sip = np.argwhere(label == 0)
 	# Make sure no voxel from the background is chosen
-	dims = np.asarray(image[0].shape)
+	dims = np.maximum(nop.max(0),sip.max(0))+1
 	out = sip[~np.in1d(np.ravel_multi_index(sip.T,dims),np.ravel_multi_index(nop.T,dims))]
+
 	voxel = random.sample(out, batch)
-
-	for i in annot[1:]:
-		recortes = batch
-		if extra != 0 and i == annot[1]:
-			recortes = batch + extra
-		options = random.sample(np.argwhere(label == i), recortes)
-		voxel.extend(options)
-
+	options = random.sample(np.argwhere(label != 0), gb.patches_per_patient-batch)
+	voxel.extend(options)
 	random.shuffle(voxel)
-	im_patch = make_patches(label, voxel, image, gb.train_patches, 'train')
+	im_patch = make_patches(label, voxel, sizes, image, 'train')
 	return im_patch
 
-def parallel_patches(image_list, label_list):
-	label = read_image(label_list, 1)
-	image = []
-	for channel in range(gb.num_ch):
-		image.append(read_image(image_list[channel], 0))
-	data_t = voxel_selection(label, image)
-	return data_t
-
-def input_pipeline(image_list, label_list):
-	# List containing the batches for the train
+def input_pipeline(image_list, label_list, sizes, subepoch):
 	print "----- Extracting patches. This might take a while -----"
 	tic = time.clock()
 	data = [[] for _ in range(gb.num_paths+1)]
-	data_temp = Parallel(n_jobs = 10)(delayed(parallel_patches)(image_list[j],label_list[j]) for j in range(len(label_list)))
-	for i in range(len(data)):
-		for j in range(len(data_temp)):
-			data[i].extend(data_temp[j][i])
+	for patient in range(len(label_list)):
+		label = read_image(label_list[patient], 1)
+		image = []
+		for channel in range(gb.num_ch):
+			image.append(read_image(image_list[patient][channel], 0))
 
+		data_t = voxel_selection(label, image, sizes)
+		for i in range(len(data)-1):
+			data[i].extend(data_t[i])
+
+		if (patient+1) % 10 == 0 and patient > 0:
+			print (patient+1)*gb.patches_per_patient, "patches extracted. Total patches:", gb.patches_per_patient*len(label_list)
+	
 	# Shuffle
 	temporal = zip(*data)
 	random.shuffle(temporal)
@@ -143,65 +137,54 @@ def input_pipeline(image_list, label_list):
 
 	toc = time.clock()
 	print "Time elapsed:", toc-tic
-	return data
+	return data 
 
 def voxels_test(size_p, desfase, original):
-	center = (size_p - size_p%2)/2
-	a1 = range(center + desfase + 1, original[0] + center + desfase, size_p)
-	b1 = range(center + desfase + 1, original[-1] + center + desfase, size_p) 
-	c1 = [a1,a1,b1] # [a1,b1,b1]
+	min_diff = (size_p - size_p%2)/2
+	a1 = range(min_diff + desfase, original[0] + (min_diff-1) + desfase, size_p)
+	b1 = range(min_diff + desfase, original[-1] + (min_diff-1) + desfase, size_p) 
+	c1 = [a1,b1,b1]
 	combinations = list(itertools.product(*c1))
 	return combinations
 
-def parallel_test(voxel, image):
-	data_t = make_patches(0, voxel, image, gb.train_patches, 'test')
-	return data_t
-
-def input_test(image, patches, combinations):
-	# List containing the batches for the test
-	tic = time.clock()
+def input_test(image, sizes, patches, combinations):
+	# List containing the batch for the test
+	batch_size = patches[-1]*patches[0]
 	print "----- Extracting patches. This might take a while -----"
+	tic = time.clock()
 	data = [[] for _ in range(gb.num_paths)]
-	voxel = list_split(combinations,patches[0])
-	data_temp = Parallel(n_jobs = 10)(delayed(parallel_test)(j, image) for j in voxel)
-	for i in range(len(data)):
-		for j in range(len(data_temp)):
-			data[i].extend(data_temp[j][i])
+	for rows in range(len(combinations)/batch_size):
+		voxel = combinations[:batch_size]
+		combinations.extend(voxel)
+		del combinations[:batch_size]
+
+		data_t = make_patches(0, voxel, sizes, image, 'test')
+
+		for i in range(len(data)):
+			data[i].extend(data_t[i])
+
+		print (rows+1)*(len(voxel)), "patches extracted"
 
 	toc = time.clock()
 	print "Time elapsed:", toc-tic
 	return data
 
-def pred_reconstruction(patches, total, image, original):
-	temporal = list_split(image, patches[0])
-	axis1 = []
-	for i in range(patches[0]):
-		axis2 = list_split(temporal[i], patches[1])
-		temp_ax1 = [np.concatenate(axis2[i], axis=2) for i in range(patches[1])]
-		axis1.append(np.concatenate(temp_ax1, axis=1))
-	axis0 = np.concatenate(axis1, axis=0)
-	output = axis0[:original[0],:original[1],:original[2]]
-	return output
-
-def prob_reconstruction(patches, total, image, original):
-	new_im = np.squeeze(image)
-	output = []
-	for j in range(5):
-		output.append(pred_reconstruction(patches, total, new_im[:,:,:,:,j], original))
-	return output
-
-def list_split(lista, parts):
-	size_parts = len(lista)/parts
-	output = [lista[i:i+size_parts] for i in range(0, len(lista), size_parts)]
-	return output
-
-def affine(path):
-	image = nib.load(path)
+def save_predictions(prediction, path, outpath):
+	image = nib.load(path) # To obtain the header
+	header = image.header
 	image_affine = image.affine
-	return image_affine
 
-def save_predictions(prediction, image_affine, outpath):
-	# prediction = np.transpose(prediction, [2,1,0])
 	new_pred = nib.Nifti1Image(prediction, image_affine)
 	new_pred.set_data_dtype(np.uint8)
 	nib.save(new_pred, outpath)
+
+def save_probabilities(probability, path, outpath, name):
+	image = nib.load(path) # To obtain the header
+	header = image.header
+	image_affine = image.affine
+	temp = np.split(probability, gb.num_classes, axis=-1)
+
+	for i in range(gb.num_classes):
+		new_pred = nib.Nifti1Image(probability, image_affine)
+		new_pred.set_data_dtype(np.float32)
+		nib.save(new_pred, outpath+'Maps/'+name[:-6]+'_ProbMapClass'+str(i)+'.nii.gz')
