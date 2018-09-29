3960c5cadcf3989de5ae65a72004eae8571644a4
Laura Daza <la.daza10@uniandes.edu.co>
2018-09-29 12:54:04 -0500

    Update settings.py

--- settings.py
+++ settings.py
@@ -12,8 +12,8 @@ class gb(object):
 	sub_epochs = 20
 	batch_size = 10
 	factor = 100 # Number of patients per epoch
-	patches_per_patient = 20 # cuadrar para que sea divisible en num_classes (-1)
-	num_classes = 5 # 2 o 5, binary or all
+	patches_per_patient = 20
+	num_classes = 5 
 	num_ch = 4
 	ends = ["Channels_flair.txt", "Channels_t1.txt", "Channels_t1c.txt", "Channels_t2.txt", "GtLabels.txt"] # Last one: annotations
 	train_patches = 40 # Size Train patches
@@ -25,5 +25,5 @@ class gb(object):
 	downsample = [3, 6, 8] # Factors to downsample inputs
 	fc_layers = 2 # Number of fully connected layers
 	fc_drop = 0.5 # The probability that each element is kept
-	convolutions = [[8,4,8],[7,4,7],[6,4,6],[5,4,5],[4,4,4]] # initial convolution + layers [block]
+	convolutions = [num_ch, 30, 30, 40, 40, 40, 40] # initial convolution + layers [block]
 	conv_drop = 0.98
